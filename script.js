90e7fe21537ecbf674ac75a139ffa1101baf201b
Rebecca Dearry <89046665+K2S00@users.noreply.github.com>
2021-10-23 14:50:18 -0400

    eventlistner
    eventlistner stops form from submitting and refreshing page

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const characterAmountRange = document.getElementById
 ('characterAmountRange')
 const characterAmountNumber = document.getElementById
 ('characterAmountNumber')
+const form = document.getElementById ('passwordGeneratorForm')
 
 //Eventlistner 
 characterAmountRange.addEventListener('input', syncCharacterAmount)
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
+// this eventlistner stops form from submitting and refreshing page
+form.addEventListener('submit', e => {
+    e.preventDefault
+})
 
 //fuction using eventlistners takes event argument 
 // defining const value as the same for both (this variable cannot be reassigned)
@@ -15,5 +20,5 @@ function syncCharacterAmount (e){
     const value = e.target.value
     characterAmountRange.value = value
     characterAmountNumber.value = value
-
 }
+
